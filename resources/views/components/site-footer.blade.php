69603f14c82aa96460ca7bf0f9a1e039cda29404
Tu Pham Huu <144118309+phtuws@users.noreply.github.com>
2023-10-24 16:52:50 +0700

    be

---
+++ resources/views/components/site-footer.blade.php
@@ -0,0 +1,35 @@
+<div class="container">
+    <div class="footer-content pt-5 pb-5">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                <div class="footer-widget">
+                    <div class="footer-widget-heading">
+                        <h3>CHÍNH SÁCH CỬA HÀNG</h3>
+                    </div>
+                    <ul>
+                        <x-site-footer-menu />
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xl-8 col-lg-8 col-md-6 mb-30 d-flex justify-content-end">
+                <div class="footer-widget">
+                    <div class="footer-widget-heading">
+                        <h3>THÔNG TIN LIÊN HỆ</h3>
+                        <p>Hot line: 0375053087</p>
+                        <p>Email: phamhuutu2003@gmail.com</p>
+                    </div>
+                    <div class="footer-social-icon">
+                        <h3>Theo dõi chúng tôi</h3>
+                        <a href="https://www.facebook.com/phtuws" target="_blank"><i
+                                class="fab fa-facebook facebook-bg"></i></a>
+                        <a href="https://www.instagram.com/phtuws" target="_blank"><i
+                                class="fab fa-instagram instagram-bg"></i></a>
+                        <a href="https://www.youtube.com/channel/UCekFNBjHTsvCASXrXbNTn0Q" target="_blank"><i
+                                class="fab fa-youtube youtube-bg"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright"><p>© 2023 bản quyền thuộc về Yamaha</p></div>
+        </div>
+    </div>
+</div>
